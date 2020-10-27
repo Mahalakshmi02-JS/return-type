0fe3c46fe326c354a12f0f05e2fc75cf77c8de8d
Mahalakshmi02-JS <72906335+Mahalakshmi02-JS@users.noreply.github.com>
2020-10-27 23:34:39 +0530

    returntype commit

---
+++ AgeGraph.java
@@ -0,0 +1,7 @@
+public class AgeGraph{
+	public static void main(String[]enter){
+		System.out.println("JVM invoked main method");
+		String value=Age.AgeDefnition(37);
+		System.out.println("Exit:main");
+}
+}
