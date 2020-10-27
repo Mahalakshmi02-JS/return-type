18c506bc344260a87ea23b8e0d31c298b41866f1
Mahalakshmi02-JS <72906335+Mahalakshmi02-JS@users.noreply.github.com>
2020-10-27 23:37:33 +0530

    Add files via upload

---
+++ Progress.java
@@ -0,0 +1,32 @@
+public class Progress{
+	public static String report(double percentage)
+	{
+		System.out.println("report invoked");
+		System.out.println("percentage = "+percentage);
+		if(percentage>90 && percentage<=100){
+			System.out.println("Grade A+");
+			return "Grade A+";
+		}
+		if(percentage>80 && percentage<=90){
+			System.out.println("Grade B");
+			return "Grade B";
+		}
+		if(percentage>65 && percentage<=80){
+			System.out.println("Grade C");
+			return "Grade C";
+		}
+		if(percentage>45 && percentage<=65){
+			System.out.println("Grade D");
+			return "Grade D";
+		}
+		if(percentage>30 && percentage<45){
+			System.out.println("Grade E");
+			return "Grade E";
+		}
+		if(percentage<30){
+			System.out.println("Fail");
+			return "Fail";
+		}
+		return "exit";
+	}
+}
