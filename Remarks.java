1f95db56fb6721a84496d92d65802a9cfd32804e
Mahalakshmi02-JS <72906335+Mahalakshmi02-JS@users.noreply.github.com>
2020-10-27 23:36:55 +0530

    Add files via upload

---
+++ Remarks.java
@@ -0,0 +1,7 @@
+public class Remarks{
+	public static void main(String[]system){
+		System.out.println("JVM invoked main method");
+		String value=Progress.report(89.25);
+		System.out.println("Exit:main");
+}
+}
